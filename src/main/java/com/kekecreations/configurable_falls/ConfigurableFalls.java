03d6da718c71608cdce10c0d85c0ecb1172e9cf1
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2023-10-08 01:14:28 +0100

    V0.1.1 1.16.5x Port Done

--- src/main/java/com/kekecreations/configurable_falls/ConfigurableFalls.java
+++ src/main/java/com/kekecreations/configurable_falls/ConfigurableFalls.java
@@ -2,10 +2,8 @@ package com.kekecreations.configurable_falls;
 
 import com.kekecreations.configurable_falls.config.ConfigurableFallsCommonConfigs;
 import com.kekecreations.configurable_falls.events.KekeFallEvent;
-import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -15,13 +13,14 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(ConfigurableFalls.MOD_ID)
 public class ConfigurableFalls
 {
     public static final String MOD_ID = "configurable_falls";
-    private static final Logger LOGGER = LogUtils.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public ConfigurableFalls()
     {
@@ -44,12 +43,6 @@ public class ConfigurableFalls
     {
     }
 
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-    }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
--- src/main/java/com/kekecreations/configurable_falls/mixin/HayBlockMixin.java
+++ src/main/java/com/kekecreations/configurable_falls/mixin/HayBlockMixin.java
@@ -1,11 +1,10 @@
 package com.kekecreations.configurable_falls.mixin;
 
 import com.kekecreations.configurable_falls.config.ConfigurableFallsCommonConfigs;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.HayBlock;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.block.HayBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class HayBlockMixin {
 
     @Inject(method = "fallOn", at = @At("HEAD"), cancellable = true)
-    public void fallOn(Level p_153362_, BlockState p_153363_, BlockPos p_153364_, Entity p_153365_, float p_153366_, CallbackInfo ci) {
-        p_153365_.causeFallDamage(p_153366_, ConfigurableFallsCommonConfigs.HAY_BALE_FALL_DAMAGE_PERCENTAGE.get().floatValue(), p_153362_.damageSources().fall());
+    public void fallOn(World p_180658_1_, BlockPos p_180658_2_, Entity p_180658_3_, float p_180658_4_, CallbackInfo ci) {
+        p_180658_3_.causeFallDamage(p_180658_4_, ConfigurableFallsCommonConfigs.HAY_BALE_FALL_DAMAGE_PERCENTAGE.get().floatValue());
     }
 }
