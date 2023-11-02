eaff42d9f52694d19f60e891b3829d1b0c43e2aa
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2023-11-02 13:06:53 +0000

    1.19.2 update

--- src/main/java/com/kekecreations/configurable_falls/util/ConfigurableFallsTags.java
+++ src/main/java/com/kekecreations/configurable_falls/util/ConfigurableFallsTags.java
@@ -1,18 +1,19 @@
 package com.kekecreations.configurable_falls.util;
 
 import com.kekecreations.configurable_falls.ConfigurableFalls;
-import net.minecraft.block.Block;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.Tags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
 
 public class ConfigurableFallsTags {
 
 
-    public static final Tags.IOptionalNamedTag<Block> FRAGILE
+    public static final TagKey<Block> FRAGILE
             = tag("fragile");
 
-    private static Tags.IOptionalNamedTag<Block> tag(String name) {
-        return BlockTags.createOptional(new ResourceLocation(ConfigurableFalls.MOD_ID, name));
+
+    private static TagKey<Block> tag(String name) {
+        return BlockTags.create(new ResourceLocation(ConfigurableFalls.MOD_ID, name));
     }
 }
