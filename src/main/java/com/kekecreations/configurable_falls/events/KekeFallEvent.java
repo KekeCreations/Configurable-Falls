a52fd6d4488b63072974a261987b7920a9d5baec
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2024-02-01 09:05:01 +0000

    Fall Event Ported

--- src/main/java/com/kekecreations/configurable_falls/events/KekeFallEvent.java
+++ src/main/java/com/kekecreations/configurable_falls/events/KekeFallEvent.java
@@ -7,6 +7,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraftforge.common.data.ForgeBlockTagsProvider;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
@@ -21,57 +22,56 @@ public class KekeFallEvent {
         Entity entity = livingFallEvent.getEntity();
         BlockPos layerPos = new BlockPos(entity.getBlockX(), entity.getBlockY(), entity.getBlockZ());
         BlockPos blockPos = new BlockPos(entity.getBlockX(), entity.getBlockY() - 1, entity.getBlockZ());
+        //Entity getBlockStateOn Method has protected access in 1.18.2
+        BlockState getBlockStateOn = entity.level.getBlockState(entity.getOnPos());
 
 
-        if (!entity.level().isClientSide()) {
+        if (!entity.level.isClientSide()) {
             //Ice
-            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.ICE_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.ICE_BREAK_ON_FALL.get() && entity.getBlockStateOn().getBlock() == Blocks.ICE) {
-                entity.level().destroyBlock(blockPos, true, entity);
+            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.ICE_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.ICE_BREAK_ON_FALL.get() && getBlockStateOn.getBlock() == Blocks.ICE) {
+                entity.level.destroyBlock(blockPos, true, entity);
             }
             //Glass
-            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.GLASS_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.GLASS_BREAK_ON_FALL.get() && entity.getBlockStateOn().getBlock() instanceof AbstractGlassBlock) {
-                entity.level().destroyBlock(blockPos, ConfigurableFallsCommonConfigs.GLASS_BREAK_ON_FALL_DROPS.get(), entity);
+            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.GLASS_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.GLASS_BREAK_ON_FALL.get() && getBlockStateOn.getBlock() instanceof AbstractGlassBlock) {
+                entity.level.destroyBlock(blockPos, ConfigurableFallsCommonConfigs.GLASS_BREAK_ON_FALL_DROPS.get(), entity);
             }
-            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.GLASS_PANE_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.GLASS_PANE_BREAK_ON_FALL.get() && entity.getBlockStateOn().getBlock() == Blocks.GLASS_PANE) {
-                entity.level().destroyBlock(blockPos, false, entity);
+            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.GLASS_PANE_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.GLASS_PANE_BREAK_ON_FALL.get() && getBlockStateOn.getBlock() == Blocks.GLASS_PANE) {
+                entity.level.destroyBlock(blockPos, false, entity);
             }
-            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.GLASS_PANE_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.GLASS_PANE_BREAK_ON_FALL.get() && entity.getBlockStateOn().getBlock() instanceof StainedGlassPaneBlock) {
-                entity.level().destroyBlock(blockPos, false, entity);
+            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.GLASS_PANE_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.GLASS_PANE_BREAK_ON_FALL.get() && getBlockStateOn.getBlock() instanceof StainedGlassPaneBlock) {
+                entity.level.destroyBlock(blockPos, false, entity);
             }
             //Snow
-            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.SNOW_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.SNOW_BREAK_ON_FALL.get() && entity.level().getBlockState(layerPos).getBlock() == Blocks.SNOW) {
-                entity.level().destroyBlock(layerPos, ConfigurableFallsCommonConfigs.SNOW_BREAK_ON_FALL_DROPS.get(), entity);
+            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.SNOW_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.SNOW_BREAK_ON_FALL.get() && entity.level.getBlockState(layerPos).getBlock() == Blocks.SNOW) {
+                entity.level.destroyBlock(layerPos, ConfigurableFallsCommonConfigs.SNOW_BREAK_ON_FALL_DROPS.get(), entity);
             }
 
             //Grass
-            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.GRASS_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.GRASS_BREAK_ON_FALL.get() && entity.level().getBlockState(layerPos).getBlock() == Blocks.GRASS) {
-                entity.level().destroyBlock(layerPos, ConfigurableFallsCommonConfigs.GRASS_BREAK_ON_FALL_DROPS.get(), entity);
+            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.GRASS_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.GRASS_BREAK_ON_FALL.get() && entity.level.getBlockState(layerPos).getBlock() == Blocks.GRASS) {
+                entity.level.destroyBlock(layerPos, ConfigurableFallsCommonConfigs.GRASS_BREAK_ON_FALL_DROPS.get(), entity);
             }
-            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.GRASS_TURNS_TO_DIRT_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.GRASS_TURNS_TO_DIRT_ON_FALL.get() && entity.level().getBlockState(blockPos).getBlock() == Blocks.GRASS_BLOCK) {
-                entity.level().setBlock(blockPos, Blocks.DIRT.defaultBlockState(), 3);
+            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.GRASS_TURNS_TO_DIRT_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.GRASS_TURNS_TO_DIRT_ON_FALL.get() && entity.level.getBlockState(blockPos).getBlock() == Blocks.GRASS_BLOCK) {
+                entity.level.setBlock(blockPos, Blocks.DIRT.defaultBlockState(), 3);
             }
-            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.TALL_GRASS_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.TALL_GRASS_BREAK_ON_FALL.get() && entity.level().getBlockState(layerPos).getBlock() == Blocks.TALL_GRASS) {
-                entity.level().destroyBlock(layerPos, ConfigurableFallsCommonConfigs.TALL_GRASS_BREAK_ON_FALL_DROPS.get(), entity);
+            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.TALL_GRASS_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.TALL_GRASS_BREAK_ON_FALL.get() && entity.level.getBlockState(layerPos).getBlock() == Blocks.TALL_GRASS) {
+                entity.level.destroyBlock(layerPos, ConfigurableFallsCommonConfigs.TALL_GRASS_BREAK_ON_FALL_DROPS.get(), entity);
             }
 
             //LEAVES
-            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.LEAVES_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.LEAVES_BREAK_ON_FALL.get() && entity.level().getBlockState(blockPos).is(BlockTags.LEAVES)) {
-                entity.level().destroyBlock(blockPos, ConfigurableFallsCommonConfigs.LEAVES_BREAK_ON_FALL_DROPS.get(), entity);
+            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.LEAVES_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.LEAVES_BREAK_ON_FALL.get() && entity.level.getBlockState(blockPos).is(BlockTags.LEAVES)) {
+                entity.level.destroyBlock(blockPos, ConfigurableFallsCommonConfigs.LEAVES_BREAK_ON_FALL_DROPS.get(), entity);
             }
 
 
             //ETC
-            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.MELON_EXPLODE_INTO_SLICES_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.MELON_EXPLODE_INTO_SLICES_ON_FALL.get() && entity.level().getBlockState(blockPos).getBlock() == Blocks.MELON) {
-                entity.level().destroyBlock(blockPos, true, entity);
-            }
-            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.SUS_BLOCKS_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.SUS_BLOCKS_BREAK_ON_FALL.get() && entity.level().getBlockState(blockPos).getBlock() instanceof BrushableBlock) {
-                entity.level().destroyBlock(blockPos, true, entity);
+            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.MELON_EXPLODE_INTO_SLICES_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.MELON_EXPLODE_INTO_SLICES_ON_FALL.get() && entity.level.getBlockState(blockPos).getBlock() == Blocks.MELON) {
+                entity.level.destroyBlock(blockPos, true, entity);
             }
 
 
             //TAGS
-            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.CONFIGURABLE_FALLS_FRAGILE_BREAK_ON_FALL_FALL_DISTANCE.get() && entity.level().getBlockState(blockPos).is(ConfigurableFallsTags.FRAGILE)) {
-                entity.level().destroyBlock(blockPos, ConfigurableFallsCommonConfigs.CONFIGURABLE_FALLS_FRAGILE_BREAK_ON_FALL_DROPS.get(), entity);
+            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.CONFIGURABLE_FALLS_FRAGILE_BREAK_ON_FALL_FALL_DISTANCE.get() && entity.level.getBlockState(blockPos).is(ConfigurableFallsTags.FRAGILE)) {
+                entity.level.destroyBlock(blockPos, ConfigurableFallsCommonConfigs.CONFIGURABLE_FALLS_FRAGILE_BREAK_ON_FALL_DROPS.get(), entity);
             }
         }
     }
