18b0a3adf5f1bcc07341dfae3979143ef8aa046a
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2024-02-02 22:13:05 +0000

    V1.0.0 Tags Ported

--- src/main/java/com/kekecreations/configurable_falls/util/ConfigurableFallsTags.java
+++ src/main/java/com/kekecreations/configurable_falls/util/ConfigurableFallsTags.java
@@ -3,8 +3,10 @@ package com.kekecreations.configurable_falls.util;
 import com.kekecreations.configurable_falls.ConfigurableFalls;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.material.Fluid;
 
 public class ConfigurableFallsTags {
 
@@ -12,7 +14,30 @@ public class ConfigurableFallsTags {
     public static final TagKey<Block> FRAGILE
             = tag("fragile");
 
+    public static final TagKey<Block> ICE
+            = tag("ice");
+    public static final TagKey<Block> GRASS_BLOCKS
+            = tag("grass_blocks");
+
+    public static final TagKey<Block> GRASS
+            = tag("grass");
+
+    public static final TagKey<Block> TALL_GRASS
+            = tag("tall_grass");
+
+    public static final TagKey<Block> SNOW
+            = tag("snow");
+
+    public static final TagKey<Block> MELON_BLOCKS
+            = tag("melon_blocks");
+
+    public static final TagKey<Fluid> WATER
+            = fluidTag("water");
+
     private static TagKey<Block> tag(String name) {
         return BlockTags.create(new ResourceLocation(ConfigurableFalls.MOD_ID, name));
+    }
+    private static TagKey<Fluid> fluidTag(String name) {
+        return FluidTags.create(new ResourceLocation(ConfigurableFalls.MOD_ID, name));
     }
 }
