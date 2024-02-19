faed34e0d84a9eceb4bac80ad8c696cfa33c519d
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2024-02-19 23:39:15 +0000

    V2.0.0 backported

--- src/main/java/com/kekecreations/configurable_falls/events/KekeFallEvent.java
+++ src/main/java/com/kekecreations/configurable_falls/events/KekeFallEvent.java
@@ -53,6 +53,15 @@ public class KekeFallEvent {
                 entity.level.destroyBlock(layerPos, ConfigurableFallsCommonConfigs.TALL_GRASS_BREAK_ON_FALL_DROPS.get(), entity);
             }
 
+            //FLOWERS
+            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.SMALL_FLOWERS_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.SMALL_FLOWERS_BREAK_ON_FALL.get() && entity.level.getBlockState(layerPos).is(BlockTags.SMALL_FLOWERS)) {
+                entity.level.destroyBlock(layerPos, ConfigurableFallsCommonConfigs.SMALL_FLOWERS_BREAK_ON_FALL_DROPS.get(), entity);
+            }
+
+            if (entity.fallDistance >= ConfigurableFallsCommonConfigs.TALL_FLOWERS_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.TALL_FLOWERS_BREAK_ON_FALL.get() && entity.level.getBlockState(layerPos).is(BlockTags.TALL_FLOWERS)) {
+                entity.level.destroyBlock(layerPos, ConfigurableFallsCommonConfigs.TALL_FLOWERS_BREAK_ON_FALL_DROPS.get(), entity);
+            }
+
             //LEAVES
             if (entity.fallDistance >= ConfigurableFallsCommonConfigs.LEAVES_BREAK_ON_FALL_FALL_DISTANCE.get() && ConfigurableFallsCommonConfigs.LEAVES_BREAK_ON_FALL.get() && entity.level.getBlockState(blockPos).is(BlockTags.LEAVES)) {
                 entity.level.destroyBlock(blockPos, ConfigurableFallsCommonConfigs.LEAVES_BREAK_ON_FALL_DROPS.get(), entity);
