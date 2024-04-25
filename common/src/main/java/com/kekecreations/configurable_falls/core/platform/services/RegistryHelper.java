738c25cc41b1811695036a2286ea40e60faa3b4c
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2024-04-25 19:49:46 +0100

    Services Setup

---
+++ common/src/main/java/com/kekecreations/configurable_falls/core/platform/services/RegistryHelper.java
@@ -0,0 +1,9 @@
+package com.kekecreations.configurable_falls.core.platform.services;
+
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+
+public interface RegistryHelper {
+
+    TagKey<Block> create(String $$0);
+}
