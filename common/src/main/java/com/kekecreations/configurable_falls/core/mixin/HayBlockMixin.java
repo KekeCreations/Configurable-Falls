2b9c84195af284e5f930cd2357c17745b9ed52c0
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2025-09-09 12:35:40 +0100

    Ported Fabric Fall Features

--- common/src/main/java/com/kekecreations/configurable_falls/core/mixin/HayBlockMixin.java
+++ common/src/main/java/com/kekecreations/configurable_falls/core/mixin/HayBlockMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 public class HayBlockMixin {
 
     @ModifyArg(method = "fallOn", at = @At(value = "INVOKE", target = "net/minecraft/world/entity/Entity.causeFallDamage (FFLnet/minecraft/world/damagesource/DamageSource;)Z"), index = 1)
-    private float configurable_falls_modifyFloatValue(float $$0) {
+    private float configurable_falls$modifyFloatValue(float $$0) {
         return ConfigurableFallsCommonConfig.HAY_BALE_FALL_DAMAGE_PERCENTAGE.get().floatValue();
     }
 }
