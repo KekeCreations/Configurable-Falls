602c46392e7759e3020c7250d773641a3ebb3e36
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2025-09-09 12:45:07 +0100

    Ported NeoForge Fall Event

--- neoforge/src/main/java/com/kekecreations/configurable_falls/NeoForgeConfigurableFalls.java
+++ neoforge/src/main/java/com/kekecreations/configurable_falls/NeoForgeConfigurableFalls.java
@@ -2,10 +2,12 @@ package com.kekecreations.configurable_falls;
 
 
 import com.kekecreations.configurable_falls.core.config.ConfigurableFallsCommonConfig;
+import com.kekecreations.configurable_falls.event.KekeFallEvent;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(ConfigurableFalls.MOD_ID)
 public class NeoForgeConfigurableFalls {
@@ -14,6 +16,6 @@ public class NeoForgeConfigurableFalls {
         modContainer.registerConfig(ModConfig.Type.COMMON, ConfigurableFallsCommonConfig.SPEC);
 
         ConfigurableFalls.init();
-
+        NeoForge.EVENT_BUS.addListener(KekeFallEvent::onFall);
     }
 }
